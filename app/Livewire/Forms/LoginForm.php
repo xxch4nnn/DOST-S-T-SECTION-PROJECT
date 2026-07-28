093f5e89f07685e31678af868db04f6bc9726c6a
xxch4nnn <johned623@gmail.com>
2026-07-28 15:51:32 +0800

    chore(fullstack): FS-08/FS-09/FS-14/FS-15 hardening + handoff runbook

--- app/Livewire/Forms/LoginForm.php
+++ app/Livewire/Forms/LoginForm.php
@@ -14,7 +14,7 @@ use Livewire\Form;
 
 class LoginForm extends Form
 {
-    #[Validate('required|string')]
+    #[Validate('required|email')]
     public string $email = '';
 
     #[Validate('required|string')]
